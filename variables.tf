d3430421c5f71687735cba45e29d84451c13b101
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-23 09:31:33 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,62 @@
+# High Level AWS Variables
+variable "region" {
+  description = "The primary AWS region to deploy resources to"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "aws_partition" {
+  description = "AWS partition to use for ARNs and policies"
+  type        = string
+  default     = "aws"
+}
+
+# High Level Databricks Variables
+variable "databricks_account_id" {
+  description = "ID of the Databricks account to deploy to."
+  type        = string
+  sensitive   = true
+}
+
+# Customer-managed VPC Networking Configuration
+variable "vpc_id" {
+  description = "Custom VPC ID"
+  type        = string
+  default     = null
+}
+
+variable "private_backend_subnet_config" {
+  description = "List of custom private subnet IDs"
+  type        = map(list(string))
+  default     = null
+}
+
+variable "private_route_table_id" {
+  description = "ID of the private route table to associate the backend PrivateLink endpoint subnets with"
+  type        = string
+  default     = null
+}
+
+variable "sg_egress_ports" {
+  description = "List of egress ports for security groups."
+  type        = list(string)
+  nullable    = true
+  default     = [null]
+}
+
+# Common variables to be applied to a large number of resources
+variable "resource_prefix" {
+  description = "Prefix for the resource names."
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9-.]{1,26}$", var.resource_prefix))
+    error_message = "Invalid resource prefix. Allowed 40 characters containing only a-z, 0-9, -, ."
+  }
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
