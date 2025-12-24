9c4f86c35043e22f1749aedf317619c5fc1a48b8
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-24 07:30:49 +0000

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,19 +1,20 @@
 locals {
   azs = data.aws_availability_zones.available.names
 
-  aws_partition = "aws"
+  aws_partition         = "aws"
+  assume_role_partition = "aws"
 
   databricks_provider_host = "https://accounts.cloud.databricks.com"
 
-  databricks_aws_account_id = "414351767826"
-
-  databricks_artifact_and_sample_data_account_id = "414351767826"
-
+  databricks_aws_account_id       = "414351767826"
   databricks_ec2_image_account_id = "601306020600"
 
-  assume_role_partition = "aws"
+  databricks_artifact_and_sample_data_account_id = "414351767826"
 
   unity_catalog_iam_arn = "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL"
 
   cmk_admin_value = var.cmk_admin_arn == null ? "arn:aws:iam::${var.aws_account_id}:root" : var.cmk_admin_arn
+
+  uc_iam_role        = "${var.resource_prefix}-catalog"
+  uc_catalog_name_us = replace(var.uc_catalog_name, "-", "_")
 }
