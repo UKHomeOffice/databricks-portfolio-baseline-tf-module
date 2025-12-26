39f7862a23cc6991065d4cd9a1349a1ebecce8ab
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-26 15:13:01 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "security_group_backend_vpce_id" {
   value       = aws_security_group.databricks_backend_vpce.id
 }
 
+output "workspace_storage_key_arn" {
+  description = "ARN of the KMS key used for workspace storage encryption and decryption operations"
+  value       = aws_kms_key.databricks_workspace_storage.arn
+}
+
 output "databricks_workspace_storage_key_id" {
   description = "ID of the Databricks Encryption Key Configuration used for workspace storage"
   value       = databricks_mws_customer_managed_keys.workspace_storage.customer_managed_key_id
